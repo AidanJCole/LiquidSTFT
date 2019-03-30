cd9d9559cc67feb98209c6711b90c4d22c6ba9ef
Aidan Cole <aidanjcole@gmail.com>
2019-03-30 17:55:42 -0400

    Db testing

--- LSTFT.py
+++ LSTFT.py
@@ -5,7 +5,7 @@
 from scipy.signal import stft, istft
 from PIL import Image
 from Lib import wave
-from math import sin, pi, atan2, sqrt
+from math import sin, pi, atan2, sqrt, log10
 
 def sinebow(t):
     r = 127.5*sin(2*pi*t)+127.5
@@ -23,12 +23,26 @@ print(f"Channels: {file.getnchannels()}\nByte width: {file.getsampwidth()}\nFram
 
 test = list(memoryview(file.readframes(file.getnframes())).cast("h"))
 
-f, n, Zxx = stft(test, window="hann", nperseg=256)
+f, n, Zxx = stft(test, window="hann", nperseg=512)
 
-graphC = Image.new('RGB', Zxx.shape, 0)
-graph = graphC.load()
+#graphC = Image.new('RGB', Zxx.shape, 0)
+#graph = graphC.load()
+max = 0
+min = 5000000000
 for x in range(Zxx.shape[0]):
     for y in range(Zxx.shape[1]):
-        graph[x,y] = sinebow((atan2(Zxx[x,y].real, Zxx[x,y].imag)+pi/2)/pi)
+        #colorPhase = sinebow((atan2(Zxx[x,y].real, Zxx[x,y].imag)+pi/2)/pi)
+        # maximum/min amp: 5363.286519380089 0.0
+        magnitude = sqrt(Zxx[x,y].real**2 + Zxx[x,y].imag**2)
+        temp = magnitude/32767
+        if temp > 327.67:
+            db = 20 * log10(temp)
+        else:
+            db = -40
+        max = db if max < db else max
+        min = db if min > db else min
+        #graph[x,y] = colorPhase
 
-graphC.save("test.png", "PNG")
+#graphC.save("test.png", "PNG")
+
+print(max, min)
